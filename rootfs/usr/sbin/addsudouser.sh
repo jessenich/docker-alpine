f252261a2e6d627bb3452fc9ea68ffeaf23f0fdc
Jesse N <jesse@keplerdev.com>
2021-10-18 00:16:04 -0400

    fixed tag format
    Signed-off-by: Jesse N <jesse@keplerdev.com>

--- rootfs/usr/sbin/addsudouser.sh
+++ rootfs/usr/sbin/addsudouser.sh
@@ -3,21 +3,15 @@
 # shellcheck shell=bash disable=SC2154,SC2002
 
 mksudo_init_nopasswd_sudoers() {
-    groupadd sudo
-    addgroup root sudo
+    if ! cat /etc/group | grep -o sudo; then
+        groupadd sudo
+    fi
+
     chown :sudo /etc/shadow;
     chmod 0640 /etc/shadow;
     mkdir -p /etc/sudoers.d;
 
-    # I'm unsure why this fuckery is required, but I'm not screwing with it anymore.
-    # 1. Modify file stream, make sudo group passwordless
-    # 2. Write modified stream to temporary .mod file
-    # 3. Force rm /etc/sudoers.
-    # 4. Rename our .mod to the original name.
-    # 5. Genius fuckery that works.
-    cat /etc/sudoers | sed 's/# %sudo.*/%sudo ALL=\(ALL\) NOPASSWD: ALL/g' >> /etc/sudoers.mod
-    rm -f /etc/sudoers;
-    mv /etc/sudoers.mod /etc/sudoers
+    cat /etc/sudoers | sed 's/# %sudo.*/%sudo   ALL=\(ALL\) NOPASSWD: ALL/g' | tee /etc/sudoers >/dev/null
     return 0;
 }
 
